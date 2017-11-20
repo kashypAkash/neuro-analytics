27e83a91826ef2c6bee6cef0d4830a9ae65a8cee
Madhuri Motoori <mmotoori@vmware.com>
2017-11-20 01:08:28 -0800

    Fixed model name field bug

--- web_application/public/javascripts/adminController.js
+++ web_application/public/javascripts/adminController.js
@@ -3,22 +3,22 @@ app.controller('admincontroller',['$scope','$http','$state','$cookies',function 
 
     $scope.control = "";
     $scope.adminValidate = function(){
-       /* $http.post(
-            host + 'api/v1/adminValidate',
+        console.log("In admin controller");
+        $http.post(
+             'http://localhost:5000/api/v1/adminValidate',
             {
                 username:$scope.username,
                 password:$scope.password
             },
             { cors:true}
-        )
+            )
             .success(function(data){
-
                 if(data.statusCode == 200)
-                {*/
+                {
                     $cookies.put('username',$scope.username);
                     $cookies.put('isadmin','admin');
                     $state.go('adminHome',{'test':$scope.username});
-/*                }
+               }
                 else
                 {
 
@@ -27,6 +27,6 @@ app.controller('admincontroller',['$scope','$http','$state','$cookies',function 
             })
             .error(function(data){
                 console.log('error');
-            })*/
+            })
     }
 }]);
