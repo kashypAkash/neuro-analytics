e828f6a1b725223849d82d0dc2f4a61d7acb78c1
rvfunky <raghavendra.kolisetty@servicenow.com>
2017-12-01 04:54:16 +0000

    added required code for syncadapter

---
+++ Neuro_Android/app/src/main/java/com/example/raghavendrkolisetty/neuro_android/SyncAdapter.java
@@ -0,0 +1,59 @@
+package com.example.raghavendrkolisetty.neuro_android;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by raghavendr.kolisetty on 11/27/17.
+ */
+
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+    // Global variables
+    // Define a variable to contain a content resolver instance
+    ContentResolver mContentResolver;
+
+    /**
+     * Set up the sync adapter
+     */
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+        Log.i("synctest", "ReaderSyncAdapter created");
+    }
+
+    /**
+     * Set up the sync adapter. This form of the
+     * constructor maintains compatibility with Android 3.0
+     * and later platform versions
+     */
+    public SyncAdapter(
+            Context context,
+            boolean autoInitialize,
+            boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+
+        Log.i("synctest", "ReaderSyncAdapter created");
+
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        Log.i("neuro", "reyy onPerformSync() was called");
+    }
+}
